b2a62a9e29e4c8518da05e9fc63306e0db8a0560
Chester Keh <rpchester18@gmail.com>
2023-12-22 01:09:56 +0800

    submission

--- FaaFlightCharts/FAAFlightChart/src/App.jsx
+++ FaaFlightCharts/FAAFlightChart/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         </Link>
         <NavLink
           to="About2"
-          className="nav-link"
+          className="-link"
         >
           About
         </NavLink>
