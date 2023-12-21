31e7750b8a5a964efc46c35bb96aaababd62776a
Chester Keh <rpchester18@gmail.com>
2023-12-22 01:01:00 +0800

    assignment

--- FaaFlightCharts/FAAFlightChart/src/pages/Homepage.jsx
+++ FaaFlightCharts/FAAFlightChart/src/pages/Homepage.jsx
@@ -1,3 +1,49 @@
+import "../components/Styling/Homepage.css";
+
 export default function Homepage({ flightData }) {
-  return <div></div>;
+  return (
+    <div className="Content">
+      <h4>About Flight Tracker</h4>
+
+      <p>
+        Welcome to Flight Tracker, your go-to platform for real-time flight
+        information and tracking. Our mission is to provide users with a
+        seamless and intuitive experience when it comes to monitoring flights
+        and staying informed about aviation activities.
+      </p>
+      <h3>Our Features</h3>
+      <h4>Live Flight Data</h4>
+
+      <p>
+        Flight Tracker utilizes the latest aviation data to bring you real-time
+        updates on flights worldwide. Our comprehensive database covers a wide
+        range of airlines, airports, and flight details, ensuring that you have
+        access to the most accurate and up-to-date information.
+      </p>
+
+      <h4>Saved Flights</h4>
+
+      <p>
+        Keep a record of your favorite flights with our "Save" feature. By
+        creating a personalized list of saved flights, you can easily revisit
+        and track them without the need for repeated searches.
+      </p>
+      <h4>Testing and Development</h4>
+
+      <p>
+        Please note that during the development phase, our application may
+        require the use of HTTP (insecure content) for testing purposes. To
+        facilitate a smooth testing experience, we kindly ask you to adjust your
+        site settings to allow permission for insecure content.
+      </p>
+      <h5>Instructions:</h5>
+      <p>
+        Go to your Site Settings. Locate the "Insecure Content" section. Allow
+        permission for insecure content to enable testing functionality. Your
+        cooperation in adjusting these settings will help us ensure a seamless
+        testing environment and improve the overall user experience. Thank you
+        for choosing Flight Tracker. Fly high, stay informed!
+      </p>
+    </div>
+  );
 }
--- FaaFlightCharts/FAAFlightChart/src/pages/SearchFlight.jsx
+++ FaaFlightCharts/FAAFlightChart/src/pages/SearchFlight.jsx
@@ -1,7 +1,50 @@
+import FlightCompare from "../components/FlightCompare";
+import InputBar from "../components/InputBar";
+import { useState, useEffect } from "react";
+
 export default function SearchFlight() {
+  const [flightData, setFlightData] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [userInput, setUserInput] = useState({});
+
+  //!API Section //
+  const apiLimit = new URLSearchParams({
+    access_key: "4a120c97de1aa4fe4b39008d0d76bb59",
+    limit: 10,
+  });
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `http://api.aviationstack.com/v1/flights?${apiLimit}`
+      );
+      const result = await response.json();
+      setFlightData(result.data);
+    } catch (error) {
+      console.error("Error fetching flight data:", error);
+    }
+  };
+  //!API Section //
+
+  const handleSearch = (userInput) => {
+    setUserInput(userInput);
+
+    const matchedFlights = comparedFlights(userInput);
+
+    setSearchResults(matchedFlights);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
-      <h1>Find your flights</h1>
+      <InputBar onSearch={handleSearch} />
+      <FlightCompare
+        userInput={userInput}
+        flightData={flightData}
+      />
     </>
   );
 }
